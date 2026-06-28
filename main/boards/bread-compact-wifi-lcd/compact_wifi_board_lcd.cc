ee320ee1d342ba687304adef38e9821059d106d5
WMatthau0815 <90515306+WMatthau0815@users.noreply.github.com>
2026-06-28 14:59:32 +0200

    Update compact_wifi_board_lcd.cc

--- main/boards/bread-compact-wifi-lcd/compact_wifi_board_lcd.cc
+++ main/boards/bread-compact-wifi-lcd/compact_wifi_board_lcd.cc
@@ -135,6 +135,11 @@ void InitializePowerSaveTimer() {
     power_save_timer_ = new PowerSaveTimer(-1, 60, -1);
     power_save_timer_->OnEnterSleepMode([this]() {
         GetDisplay()->SetPowerSaveMode(true);
+        GetDisplay()->ClearChatMessages();
+        GetDisplay()->SetChatMessage("system", "");
+        GetDisplay()->SetEmotion("neutral");
+        GetDisplay()->SetStatus("STANDBY");
+        GetDisplay()->UpdateStatusBar(true);
         if (auto* backlight = GetBacklight()) {
             backlight->SetBrightness(1);
         }
