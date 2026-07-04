2f27411d23656b27140f51b4b958490f0ceafb3a
WMatthau0815 <90515306+WMatthau0815@users.noreply.github.com>
2026-07-04 19:21:17 +0200

    Update AnalogClock.h

--- main/display/lvgl_display/AnalogClock.h
+++ main/display/lvgl_display/AnalogClock.h
@@ -1,24 +1,22 @@
 #pragma once
 #include "lvgl.h"
+#include "display.h"    // NEU: für Display, DisplayLockGuard
 
 class AnalogClock {
 public:
-    static void Start(lv_obj_t* parent);
-    static void Stop();
+    static void Start(lv_obj_t* parent, Display* display);   // GEÄNDERT
+    static void Stop(Display* display);                       // GEÄNDERT
 private:
     static void TimerCb(lv_timer_t* timer);
     static void UpdateHands();
-    // Add these helper methods
     static void CalcHand(float deg, int length, lv_point_precise_t* pts);
     static lv_obj_t* CreateHand(lv_obj_t* parent, lv_point_precise_t* pts,
                                  uint32_t color, int width);
     static void DrawFace(lv_obj_t* parent);
-    
-    // Add these static member variables
+
     static int cx_;
     static int cy_;
-    
-    // Existing static members
+
     static lv_obj_t* sec_hand_;
     static lv_obj_t* sec_cw_;
     static lv_obj_t* min_hand_;
@@ -26,6 +24,7 @@ private:
     static lv_obj_t* center_dot_;
     static lv_obj_t* clock_container_;
     static lv_timer_t* clock_timer_;
+    static Display* display_;   // NEU
     static lv_point_precise_t sec_pts_[2];
     static lv_point_precise_t sec_cw_pts_[2];
     static lv_point_precise_t min_pts_[2];
