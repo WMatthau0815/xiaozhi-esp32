2c8b5a2cf5e5e157c74d61d45c49711b6f1f9d13
WMatthau0815 <90515306+WMatthau0815@users.noreply.github.com>
2026-07-19 19:43:56 +0200

    Update AnalogClock.h

--- main/display/lvgl_display/AnalogClock.h
+++ main/display/lvgl_display/AnalogClock.h
@@ -1,9 +1,9 @@
 #pragma once
 #include "lvgl.h"
 #include "display.h"
-#include "temperature_sensor.h"   // NEU
+//#include "temperature_sensor.h"   // NEU
 
-#define TEMP_LABEL_Y 70   // absolute Y-Position von oben, unabhängig von Uhr-Mittelpunkt
+#define TEMP_LABEL_Y 110   // for GC9A01 it's 70 - absolute Y-Position von oben, unabhängig von Uhr-Mittelpunkt
 
 class AnalogClock {
 public:
@@ -12,7 +12,7 @@ public:
 private:
     static void TimerCb(lv_timer_t* timer);
     static void UpdateHands();
-    static void UpdateTemperature();   // NEU
+//    static void UpdateTemperature();   // NEU
     static void CalcHand(float deg, int length, lv_point_precise_t* pts);
     static lv_obj_t* CreateHand(lv_obj_t* parent, lv_point_precise_t* pts,
                                  uint32_t color, int width);
@@ -24,11 +24,11 @@ private:
     static lv_obj_t* min_hand_;
     static lv_obj_t* hr_hand_;
     static lv_obj_t* center_dot_;
-    static lv_obj_t* temp_label_;        // NEU
+//    static lv_obj_t* temp_label_;        // NEU
     static lv_obj_t* clock_container_;
     static lv_timer_t* clock_timer_;
     static Display* display_;
-    static TemperatureSensor* temp_sensor_;   // NEU
+//    static TemperatureSensor* temp_sensor_;   // NEU
     static lv_point_precise_t sec_pts_[2];
     static lv_point_precise_t sec_cw_pts_[2];
     static lv_point_precise_t min_pts_[2];
